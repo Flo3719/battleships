cb55f07b18a7bf552b6f28bae4094da0ab093985
s2526360 <f.fahrenholz@student.utwente.nl>
2021-01-10 17:17:31 +0100

    fixed naming to go with conventions

--- src/GUIController.java
+++ src/GUIController.java
@@ -3,6 +3,7 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Control;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 
@@ -11,6 +12,7 @@ import java.util.List;
 
 public class GUIController {
     public GridPane enemy_grid;
+    public Label friend_name_Label;
 
     public void handleButtonClick(Event evt){
         System.out.println(((Control)evt.getSource()).getId());
