5ce237b8c08b59967f4082a878bd9bf809d32c11
koen Rienstra <koen@DESKTOP-2TM2UBL>
2021-01-19 14:13:37 +0100

    Added names for every Ship, e.g. C1, C2, P6, etc.

--- src/Battleships/PositionModel.java
+++ src/Battleships/PositionModel.java
@@ -5,6 +5,7 @@ public class PositionModel {
 	public int y;
 	public ShipModel ship;
 	public boolean hasBeenGuessed;
+	public String shipName;
 	
 	public PositionModel(int x, int y) {
 		this.x = x;
--- src/Battleships/ShipModel.java
+++ src/Battleships/ShipModel.java
@@ -6,14 +6,47 @@ public class ShipModel {
 
 	public ShipType shipType;
 	public ArrayList<PositionModel> positions = new ArrayList<>();
+	public String shipName;
+	
+	public static int counterCBoat = 1;
+	public static int counterBBoat = 1;
+	public static int counterDBoat = 1;
+	public static int counterSBoat = 1;
+	public static int counterPBoat = 1;
+	
+	public ShipModel(ShipType shipType) {
+		this.shipType = shipType;
+		switch(this.shipType) {
+		case CARRIER:
+			counterCBoat++;
+			this.shipName = shipType.identifier+counterCBoat;
+			break;
+		case BATTLESHIP:
+			counterBBoat++;
+			this.shipName = shipType.identifier+counterBBoat;
+			break;
+		case DESTROYER:
+			counterDBoat++;
+			this.shipName = shipType.identifier+counterDBoat;
+			break;
+		case SUPER_PATROL:
+			counterSBoat++;
+			this.shipName = shipType.identifier+counterSBoat;
+			break;
+		case PATROL_BOAT:
+			counterPBoat++;
+			this.shipName = shipType.identifier+counterPBoat;
+			break;
+		}
+			
+			
+			
+	}
 	
 	public int getLength() {
 		return shipType.length;
 	}
 	
-	public ShipModel(ShipType shipType) {
-		this.shipType = shipType;
-	}
 	public boolean Sunk() {
 		for(PositionModel pos : positions) {
 			if(!pos.hasBeenGuessed)
