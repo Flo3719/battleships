d0b060eda24532d8343bfa99fa2f9baf38822046
s2526360 <f.fahrenholz@student.utwente.nl>
2021-01-26 12:12:40 +0100

    tried to add more tests

--- src/Tests/GameClientHandlerTest.java
+++ src/Tests/GameClientHandlerTest.java
@@ -40,4 +40,6 @@ public class GameClientHandlerTest {
 		Board board = gch.toBoard(testBoard);
 		assertEquals(testBoard, board.toString());
 	}
+
+
 }
---
+++ src/Tests/MainViewTest.java
@@ -0,0 +1,21 @@
+package Tests;
+
+import Battleships.Controllers.ClientController;
+import Battleships.Controllers.MainViewController;
+import Battleships.Models.Board;
+import Battleships.Views.MainView;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainViewTest {
+    //TODO fix
+    @Test
+    public void testAddPlayButtons(){
+        MainView mainView = new MainView();
+        //mainView.initialize();
+        mainView.addPlayButtons(mainView.friendGrid, "friend");
+        assertEquals(3, mainView.friendGrid.getChildren().size());
+    }
+}
