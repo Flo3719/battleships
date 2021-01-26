a41a35b6226a0a3ce7cd83613a7a0707b8dd7b6e
s2526360 <f.fahrenholz@student.utwente.nl>
2021-01-26 21:44:22 +0100

    Added more tests

---
+++ src/Battleships/Tests/MainViewControllerTest.java
@@ -0,0 +1,31 @@
+package Battleships.Tests;
+
+import Battleships.Controllers.GameClientHandler;
+import Battleships.Controllers.MainViewController;
+import Battleships.Models.ShipModel;
+import Battleships.Views.MainView;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainViewControllerTest {
+    MainViewController mainViewController;
+
+    @BeforeEach
+    public void init(){
+        mainViewController = (MainViewController) MainViewController.sharedInstance;
+    }
+
+    @Test
+    public void testInitialize(){
+        MainView view = new MainView();
+        String testBoard = "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,";
+        assertEquals(mainViewController.getBoard(), testBoard);
+        mainViewController.initialize(view);
+        assertNotNull(mainViewController.getBoard());
+    }
+}
