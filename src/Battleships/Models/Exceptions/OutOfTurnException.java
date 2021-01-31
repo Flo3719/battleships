cb4493cbf52242deeebae4038aa67089bbe5cf5e
s2526360 <f.fahrenholz@student.utwente.nl>
2021-01-31 16:38:21 +0100

    added exceptions

---
+++ src/Battleships/Models/Exceptions/OutOfTurnException.java
@@ -0,0 +1,8 @@
+package Battleships.Models.Exceptions;
+
+public class OutOfTurnException extends Exception {
+    public OutOfTurnException() {
+        super("It is not your turn...");
+    }
+}
+
