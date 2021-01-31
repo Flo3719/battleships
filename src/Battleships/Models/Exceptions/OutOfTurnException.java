790e9b07b1540e0c349f73581fc0b3585ce915e1
s2526360 <f.fahrenholz@student.utwente.nl>
2021-01-31 15:25:52 +0100

    added class diagram and removed unused custom exceptions

--- src/Battleships/Models/Exceptions/OutOfTurnException.java
+++
@@ -1,7 +0,0 @@
-package Battleships.Models.Exceptions;
-
-public class OutOfTurnException extends Exception {
-	public OutOfTurnException() {
-		super("It is not your turn...");
-	}
-}
--- src/Battleships/Models/Exceptions/ServerNotAvailableException.java
+++
@@ -1,7 +0,0 @@
-package Battleships.Models.Exceptions;
-
-public class ServerNotAvailableException extends Exception{
-    public ServerNotAvailableException(String msg){
-        super(msg);
-    }
-}
