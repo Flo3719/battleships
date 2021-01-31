437898d92abcabd1139e9d5849f1814eced271bf
s2526360 <f.fahrenholz@student.utwente.nl>
2021-01-31 16:54:14 +0100

    Revert "added exceptions"
    This reverts commit cb4493cbf52242deeebae4038aa67089bbe5cf5e.

--- src/Battleships/Models/Exceptions/OutOfTurnException.java
+++
@@ -1,8 +0,0 @@
-package Battleships.Models.Exceptions;
-
-public class OutOfTurnException extends Exception {
-    public OutOfTurnException() {
-        super("It is not your turn...");
-    }
-}
-
